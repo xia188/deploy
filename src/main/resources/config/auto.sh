0094ae116ddf9d818105ec6c83a581b7c3ab0c1a
xlongwei <admin@xlongwei.com>
2021-07-20 16:43:09 +0800

    svn cleanup

--- src/main/resources/config/auto.sh
+++ src/main/resources/config/auto.sh
@@ -7,15 +7,16 @@ services=(
 for ((i = 0; i < ${#services[@]}; ++i)); do
 
     service=${services[i]}
-    # svn up -r 278 webapp
+    svn cleanup cmp_${service}
+    # svn up -r 278 cmp_order
     lines=`svn up cmp_${service}|wc -l`
     # cd cmp_${service} && lines=`git pull origin master|wc -l` && cd ..
     if [ $lines -gt 2 ] ; then
-        echo "`date +"%Y-%m-%d %H:%M.%S"` $service do deploy"
+        echo "`date +"%Y-%m-%d %H:%M:%S"` $service do deploy"
         sh deploys.sh $service
-        echo "`date +"%Y-%m-%d %H:%M.%S"` $service end deploy"
+        echo "`date +"%Y-%m-%d %H:%M:%S"` $service end deploy"
     else
-        echo "`date +"%Y-%m-%d %H:%M.%S"` $service no change"
+        echo "`date +"%Y-%m-%d %H:%M:%S"` $service no change"
     fi
 
 done
